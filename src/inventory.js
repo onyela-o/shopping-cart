64a9b7e2a930969f3b8c58c66aab73becef581d5
Santosh Dasari <santosh.dasari@digital.trade.gov.uk>
2022-04-28 17:08:43 +0100

    Add inventory of products

---
+++ src/inventory.js
@@ -0,0 +1,44 @@
+const Inventory = [
+  {
+    title: "apples",
+    images:
+      "https://www.google.co.uk/search?q=apples+images&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj85vK_j7f3AhULilwKHbmFCDQQ_AUoAXoECAEQAw&biw=1785&bih=951&dpr=2#imgrc=w8W65miCcz1jnM",
+    description: "A packet of delicious apples",
+    price: 2,
+    quantity: 2,
+  },
+  {
+    title: "bananas",
+    images:
+      "https://www.google.co.uk/search?q=apples+images&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj85vK_j7f3AhULilwKHbmFCDQQ_AUoAXoECAEQAw&biw=1785&bih=951&dpr=2#imgrc=w8W65miCcz1jnM",
+    description: "A bunch of apples",
+    price: 1,
+    quantity: 6,
+  },
+  {
+    title: "Grapes",
+    images:
+      "https://www.google.co.uk/search?q=apples+images&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj85vK_j7f3AhULilwKHbmFCDQQ_AUoAXoECAEQAw&biw=1785&bih=951&dpr=2#imgrc=w8W65miCcz1jnM",
+    description: "A Box of white grapes",
+    price: 3,
+    quantity: 2,
+  },
+  {
+    title: "Mangoes",
+    images:
+      "https://www.google.co.uk/search?q=apples+images&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj85vK_j7f3AhULilwKHbmFCDQQ_AUoAXoECAEQAw&biw=1785&bih=951&dpr=2#imgrc=w8W65miCcz1jnM",
+    description: "Imported Mangoes from South America",
+    price: 4,
+    quantity: 1,
+  },
+  {
+    title: "Pears",
+    images:
+      "https://www.google.co.uk/search?q=apples+images&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj85vK_j7f3AhULilwKHbmFCDQQ_AUoAXoECAEQAw&biw=1785&bih=951&dpr=2#imgrc=w8W65miCcz1jnM",
+    description: "British pears",
+    price: 2,
+    quantity: 4,
+  },
+];
+
+export default Inventory;
